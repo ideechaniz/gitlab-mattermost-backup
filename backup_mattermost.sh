c8fd8f5872e977f6a1b6ab0045a477a8d0680907
ideechaniz <ideechaniz@gmail.com>
2024-06-02 12:36:53 +0200

    Update backup_mattermost.sh
    remove AWS related stuff and add terminal output info about the stage it is in

--- backup_mattermost.sh
+++ backup_mattermost.sh
@@ -13,20 +13,20 @@ else
     exit 1
 fi
 
-# prepare
+echo prepare
 rm -rf $gitlabBackupDir/mattermost
 mkdir -p $gitlabBackupDir/mattermost/data
 
-# backup data
+echo backup data
 cp -R $mattermostDir/* $gitlabBackupDir/mattermost/data
 # backup postgres
 su - mattermost -c "/opt/gitlab/embedded/bin/pg_dump -U gitlab_mattermost -h /var/opt/gitlab/postgresql -p 5432 mattermost_production" > $gitlabBackupDir/mattermost/mattermost_production_backup.sql
 
-# package and cleanup
+echo package and cleanup
 backupfile=$(date +%s_%Y_%m_%d)_mattermost_backup.tar.gz
 tar -zcf $gitlabBackupDir/$backupfile $gitlabBackupDir/mattermost
 rm -rf $gitlabBackupDir/mattermost
 
 # upload to S3
-$awscli s3 cp $gitlabBackupDir/$backupfile $s3path
-rm -f $gitlabBackupDir/$backupfile
+#$awscli s3 cp $gitlabBackupDir/$backupfile $s3path
+echo result in: $gitlabBackupDir/$backupfile
